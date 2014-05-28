094f673e8701728b8eac359b3a6add2a5544d0f3
Cedric Van Bockhaven <cedric@ce3c.be>
2014-05-28 16:08:42 +0200

    skech

--- sketch/sketchscreen.js
+++ sketch/sketchscreen.js
@@ -20,36 +20,62 @@ window.onload = function (){
 
 	$($("#sketch_palette li")[0]).addClass("selected")
 
-    $('#sketch_surface').sketch();
-
-
-
-
-  })(jQuery);
-}
-
-function sendSketch(images) {
-	$.post('https://noveria.nl/sketchscreen/index.php', {images: images}, function(data) {
-		//this is the success function
-		console.log(data);
+	$('#sketch_trash')[0].addEventListener('click', function() {
+		clearSketch()
 	});
 
-}
+	$('#sketch_submit')[0].addEventListener('click', function() {
+		sendSketch([$('#sketch_surface')[0].toDataURL()])
+		clearSketch()
+	});
 
-function showSketchOn(selector) {
-	$.get('https://noveria.nl/sketchscreen/index.php', {action: 'getimage'}, function(images) {
-		delay = 0;
-		images = images.reverse();
-		while(img = images.pop()) {
-			
-			//set img src of selector to img, possibly fade
-			setTimeout(function(img, selector) { 
-				selector.fadeTo(1200, 0.3, function() {
-					$(this).css('background-image', "url(" + img + ")");
-				}).fadeTo(1200, 1);
-//				selector.css('background-image', "url(" + img + ")");
-			}, delay, img, selector);
-			delay += 4000;
+	$('#sketch_toggle a')[0].addEventListener('click', function() {
+		$('#sketch').toggle()
+		if ($("#sketch").is(":visible")) {
+			$('#sketch_toggle a i').removeClass("icon-tint").addClass("icon-arrow-down")
+		} else {
+			$('#sketch_toggle a i').removeClass("icon-arrow-down").addClass("icon-tint")
 		}
-	}, "json");
+	});
+
+    $('#sketch_surface').sketch();
+    showSketchOn($('#icongrid'))
+
+    function clearSketch() {
+		$('#sketch_surface').data('sketch').actions = []
+		var canvas = $('#sketch_surface')[0];
+		var ctx = canvas.getContext("2d");
+		ctx.beginPath()
+		ctx.clearRect(0,0,canvas.width,canvas.height);
+		ctx.save()
+    }
+
+	function sendSketch(images) {
+		$.post('https://noveria.nl/sketchscreen/index.php', {images: images}, function(data) {
+			//this is the success function
+			console.log(data);
+		});
+
+	}
+
+	function showSketchOn(selector) {
+		$.get('https://noveria.nl/sketchscreen/index.php', {action: 'getimage'}, function(images) {
+			delay = 0;
+			images = images.reverse();
+			while(img = images.pop()) {
+				
+				//set img src of selector to img, possibly fade
+				setTimeout(function(img, selector) { 
+					selector.fadeTo(1200, 0.3, function() {
+						$(this).css('background-image', "url(" + img + ")");
+					}).fadeTo(1200, 1);
+	//				selector.css('background-image', "url(" + img + ")");
+				}, delay, img, selector);
+				delay += 4000;
+			}
+		}, "json");
+		setTimeout(showSketchOn, 60000, selector)
+	}
+
+  })(jQuery);
 }
