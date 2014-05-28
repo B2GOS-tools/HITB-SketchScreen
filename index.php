3b5cb16dbb9dab6e4de8ba26d8471ead51eaa49d
Jan Laan <jan.laan@os3.nl>
2014-05-28 15:21:51 +0200

    Updates

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
-error_reporting(E_ALL);
+  header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
+    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+    header('Access-Control-Max-Age: 1000');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 $PDO = new PDO('mysql:host=localhost;dbname=sketchscreen', 'sketch', 'sketchpass123');
-
+//var_dump($_POST);
 if(isset($_POST['images'])) {
 	$stmt = $PDO->prepare("insert into images (timestamp, data) values(UNIX_TIMESTAMP(), :data)");
-	$stmt->bindValue('data', $_POST['images']);
+	$stmt->bindValue('data', json_encode($_POST['images']));
 	$stmt->execute();
 }
 else if(isset($_GET['action']) && $_GET['action'] == 'getimage') {
--- sketch/sketchscreen.js
+++ sketch/sketchscreen.js
@@ -27,3 +27,23 @@ window.onload = function (){
 
   })(jQuery);
 }
+
+function sendSketch(images) {
+	$.post('https://noveria.nl/sketchscreen/index.php', {images: images}, function(data) {
+		//this is the success function
+		console.log(data);
+	});
+
+}
+
+function showSketchOn(imgObject) {
+	$.get('https://noveria.nl/sketchscreen/index.php', {action: 'getimage'}, function(images) {
+		while(img = images.pop()) {
+			
+			//set img src of selector to img, possibly fade
+			setTimeout(function() { 
+				$('#icongrid').css('background-image', img);
+			}, 300);
+		}
+	}, "json");
+}
